6646796732920b8ec65e638bfc52ada41b9fdc64
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-12-23 00:00:03 -0600

    lifting state

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,86 @@ import CatList from './components/CatList';
 
 const DATA = [
   {
+    id: 1,
     name: 'Ubik',
     caretaker: 'Maria',
     color: 'grey',
     personality: 'wild child',
+    petCount: 0,
   },
   {
+    id: 2,
     name: 'Pepper',
     caretaker: 'Mark',
     color: 'black',
     personality: 'spicy',
+    petCount: 0,
   },
   {
+    id: 3,
     name: 'Binx',
     caretaker: 'Susan',
     color: 'tuxedo',
     personality: 'feral',
+    petCount: 0,
   },
   {
+    id: 4,
     name: 'Luna',
     caretaker: 'Jake',
     color: 'white',
     personality: 'cuddly',
+    petCount: 0,
   }
 ];
 
-
 function App() {
+  const [catData, setCatData] = useState(DATA);
+
+  const handlePetCat = (id) => {
+    // console.log(`we pet cat ${id}`);
+
+    // has to be different list, need to make a new outer object
+    // update cat
+    setCatData( catData => catData.map(cat => {
+      if (cat.id === id) {
+        // update pet count
+        return { ...cat, petCount: cat.petCount + 1 };
+      } else {
+        // no change
+        return cat;
+      }
+    }));
+  };
+
+  const calculateTotalPetCount = (catData) => {
+    let total = 0;
+    for (const cat of catData) {
+      total += cat.petCount;
+    }
+    return total;
+  };
+
+  // // using reduce
+  // const calculateTotalPetCount = (catData) => {
+  //   return catData.reduce((total, cat) => {
+  //     return total + cat.petCount;
+  //   }, 0);
+  // };
+
+  const totalPets = calculateTotalPetCount(catData);
 
   return (
     <>
       <main>
         <h1> The Cat Corral </h1>
-        <CatList catData={DATA}/>
+        <h2>Total Number of Pets Across all Cats: {totalPets}</h2>
+        <CatList catData={catData} onPetCat={handlePetCat}/>
       </main>
     </>
-
   );
 }
 
 export default App;
+
+// export default App;
--- src/components/Cat.jsx
+++ src/components/Cat.jsx
@@ -1,31 +1,39 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 
-const Cat = ({ name, color, personality, caretaker }) => {
-    const [petCount, setPetCount] = useState(0);
-    
-    const petClicked = () => {
-        setPetCount(petCount => petCount + 1);
-    };
-    
-    return(
-        <li>
-            <h3>Name: {name}</h3>
-            <h3>Color: {color}</h3>
-            <h3>Personality: {personality}</h3>
-            <h3>Caretaker: {caretaker}</h3>
-            <h3># of Pets: {petCount}</h3>
-            <p>🐈</p>
-            <button onClick={() => petClicked()}>Pet Cat</button>
-        </li>
-    );
+const Cat = ({
+  id,
+  name,
+  color,
+  personality,
+  caretaker,
+  petCount,
+  onPetCat,
+}) => {
+  const onPetClick = () => {
+    onPetCat(id);
+  };
+
+  return (
+    <li>
+      <h3>Name: {name}</h3>
+      <h3>Color: {color}</h3>
+      <h3>Personality: {personality}</h3>
+      <h3>Caretaker: {caretaker}</h3>
+      <h3># of Pets: {petCount}</h3>
+      <p>🐈</p>
+      <button onClick={onPetClick}>Pet Cat</button>
+    </li>
+  );
 };
 
 Cat.propTypes = {
-    name: PropTypes.string.isRequired,
-    color: PropTypes.string.isRequired,
-    personality: PropTypes.string.isRequired,
-    caretaker: PropTypes.string.isRequired
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  personality: PropTypes.string.isRequired,
+  caretaker: PropTypes.string.isRequired,
+  petCount: PropTypes.number.isRequired,
+  onPetCat: PropTypes.func.isRequired,
 };
 
 export default Cat;
